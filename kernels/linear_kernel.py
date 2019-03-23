e79e490c4d742546ee48bf9beb68eb7e99bb5a5a
charlesAuguste <charles.auguste@ymail.com>
2019-03-23 12:14:09 +0100

    Faster Linear kernel.

--- kernels/linear_kernel.py
+++ kernels/linear_kernel.py
@@ -2,26 +2,30 @@
 ### LinearKernel
 ########################################################################
 
-from kernels.kernel import *
+from kernels.fast_kernel import FastKernel
 import numpy as np
 
-class LinearKernel(Kernel):
-    """ LinearKernel class """
 
+class LinearKernel(FastKernel):
     def __init__(self):
-        super().__init__()
+        pass
 
-    def evaluate(self, x, y):
-        return x.dot(y.transpose())
+    def string_to_vec(self, string):
+        vec = []
+        for letter in string:
+            if letter == "A":
+                vec.extend([1, 0, 0, 0])
+            elif letter == "C":
+                vec.extend([0, 1, 0, 0])
+            elif letter == "G":
+                vec.extend([0, 0, 1, 0])
+            elif letter == "T":
+                vec.extend([0, 0, 0, 1])
 
-    def compute_train(self, Xtr):
-        print("Compute Train: Linear Kernel")
-        Ktr = Xtr.dot(Xtr.T)
-        print("end")
-        return Ktr
+        return vec
 
-    def compute_test(self, Xtr, Xte):
-        print("Compute Test: Linear Kernel")
-        K_te = Xte.dot(Xtr.T)
-        print("end")
-        return K_te
+    def compute_feature_vector(self, data):
+        feature_vector = []
+        for string in data:
+            feature_vector.append(self.string_to_vec(string))
+        return np.array(feature_vector)
