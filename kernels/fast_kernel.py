fix two things for normalization, but still low results

kernels/fast_kernel.py
@@ -1,25 +1,44 @@
 import numpy as np
+from scipy.sparse.linalg import norm
 
 from abc import abstractmethod, ABC
 class FastKernel(ABC):
-    def __init__(self):
+    def __init__(self, normalize=False):
+        self.normalize = normalize
         pass
 
     @abstractmethod
-    def compute_feature_vector(self):
+    def compute_feature_vector(self, X):
         pass
 
     def compute_train(self, data_train):
         feature_vector = self.compute_feature_vector(data_train)
         K = np.dot(feature_vector, feature_vector.T)
-        normalization = np.sqrt(K.diagonal()).reshape(-1,1).dot(np.sqrt(K.diagonal()).reshape(1,-1))
-        print("normalization train:", normalization.shape)
-        return K / normalization
+        if self.normalize:
+            K = self.normalize_train(K)
+        return K
 
     def compute_test(self, data_train, data_test):
         feature_vector_train = self.compute_feature_vector(data_train)
         feature_vector_test = self.compute_feature_vector(data_test)
         K = np.dot(feature_vector_test, feature_vector_train.T)
-        normalization = np.sqrt(K.diagonal()).reshape(-1,1).dot(np.sqrt(K.diagonal()).reshape(1,-1))
-        print("normalization test:", normalization.shape)
-        return K / normalization
+        if self.normalize:
+           K = self.normalize_test(K, feature_vector_test)
+        return K
+
+    def normalize_train(self, K_train): #K_train unormalized
+        self.norms_train = np.sqrt(K_train.diagonal())  # norms for x train vector
+        matrix_norms = np.outer(self.norms_train,self.norms_train) #10e-40
+        K_train =  np.divide(K_train, matrix_norms)
+        #print("diag", np.sum(np.diagonal(K_train)))
+        print("K_train",K_train)
+        return K_train
+
+    def normalize_test(self, K_test, feats_test): #K_test unormalized
+        m = K_test.shape[0]
+        #norms_test = np.sum(feats_test*feats_test,axis=1)
+        norms_test = norm(feats_test,axis=1)
+        matrix_norms = np.outer(norms_test,self.norms_train) #+ 1e-40  # matrix sqrt(K(xtest,xtest)*K(xtrain,xtrain))
+        K_test = np.divide(K_test, matrix_norms)
+        print("K_test",K_test)
+        return K_test

kernels/fast_spectrum_kernel.py
@@ -1,11 +1,13 @@
-from kernels.fast_kernel import FastKernel
 import numpy as np
 from scipy.sparse import csr_matrix, lil_matrix
+from kernels.fast_kernel import FastKernel
 
-class SpectrumKernel():
-    def __init__(self, k):
+class SpectrumKernel(FastKernel):
+    def __init__(self, k, normalize=False):
+        super().__init__()
         self.k = k
         self.index = {"A": 0, "C": 1, "G": 2, "T": 3}
+        self.normalize = normalize
 
     def compute_index(self, word):
         for i in range(len(word)):
@@ -22,18 +24,14 @@ class SpectrumKernel():
     def compute_train(self, data_train):
         feature_vector = self.compute_feature_vector(data_train)
         K = np.dot(feature_vector, feature_vector.T).toarray()
-
-        normalization = np.sqrt(K.diagonal()).reshape(-1,1).dot(np.sqrt(K.diagonal()).reshape(1,-1))
-        print("normalization test:", normalization.shape)
-
-        return K/normalization
+        if self.normalize:
+            K = self.normalize_train(K)
+        return K
         
     def compute_test(self, data_train, data_test):
         feature_vector_train = self.compute_feature_vector(data_train)
         feature_vector_test = self.compute_feature_vector(data_test)
         K = np.dot(feature_vector_test, feature_vector_train.T).toarray()
-
-        normalization = np.sqrt(K.diagonal()).reshape(-1,1).dot(np.sqrt(K.diagonal()).reshape(1,-1))
-        print("normalization test:", normalization.shape)
-
-        return K/normalization
+        if self.normalize:
+            K = self.normalize_test(K, feature_vector_test)
+        return K

kernels/fast_sum_spectrum_kernel.py
@@ -28,18 +28,11 @@ class SumSpectrumKernel(FastKernel):
         K = 0
         for i in range(len(self.kernels)):
             K += self.kernels[i].compute_train(data_train)
-
-        normalization = np.sqrt(K.diagonal()).reshape(-1,1).dot(np.sqrt(K.diagonal()).reshape(1,-1))
-        print("normalization test:", normalization.shape)
-
-        return K/normalization
+        return K
 
     def compute_test(self, data_train, data_test):
         K = 0
         for i in range(len(self.kernels)):
             K += self.kernels[i].compute_test(data_train, data_test)
-
-        normalization = np.sqrt(K.diagonal()).reshape(-1,1).dot(np.sqrt(K.diagonal()).reshape(1,-1))
-        print("normalization test:", normalization.shape)
-
-        return K/normalization
+            #give the option to normalize each kernel
+        return K

kernels/kernel.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 
 class Kernel(ABC):
 
-    def __init__(self, normalize=True):
+    def __init__(self, normalize=False):
         self.normalize = normalize
         return
 
@@ -33,12 +33,7 @@ class Kernel(ABC):
         K = K + K.T - np.diag(K.diagonal())
 
         if self.normalize:
-            D = np.diag(K.diagonal())
-            print(D.shape)
-            normalization_term = np.sqrt(D.dot(D.T)) + 1e-40
-            print(normalization_term)
-            K /= normalization_term
-            print("K:", K)
+            K = self.normalize_train(K)
 
         end = time.time()
         print("Time elapsed: {0:.2f}".format(end - start))
@@ -57,12 +52,33 @@ class Kernel(ABC):
         for (k,j) in tqdm(pairs):
             K_t[k, j] = self.evaluate(Xte[k], Xtr[j])
 
-
         if self.normalize:
-            D = np.diag(K_t.diagonal())
-            normalization_term = np.sqrt(D.dot(D.T))
-            K_t = np.divide(K_t, normalization_term)
+           K_t = self.normalize_test(K_t, Xte = Xte)
 
         end = time.time()
         print("Time elapsed: {0:.2f}".format(end - start))
         return K_t
+
+
+    def normalize_train(self, K_train): #K_train unormalized
+        self.norms_train = np.sqrt(K_train.diagonal())  # norms for x train vector
+        matrix_norms = np.outer(self.norms_train,self.norms_train) #10e-40
+        K_train =  np.divide(K_train, matrix_norms)
+        print("K_train",K_train)
+        return K_train
+
+    def normalize_test(self, K_test, feats_test = None, Xte = None): #K_test unormalized
+        m = K_test.shape[0]
+        if feats_test is None:
+            assert (not (Xte is None))
+            norms_test = np.zeros(m)  # norms for x test vector
+            for k in range(m):
+                norms_test[k] = self.evaluate(Xte[k], Xte[k])
+        else:
+            assert (not (feats_test is None))
+            norms_test = np.linalg.norm(feats_test,axis=1)
+        matrix_norms = np.outer(norms_test,self.norms_train) #+ 1e-40  # matrix sqrt(K(xtest,xtest)*K(xtrain,xtrain))
+        K_test = np.divide(K_test, matrix_norms)
+        print("K_test",K_test)
+        return K_test
+