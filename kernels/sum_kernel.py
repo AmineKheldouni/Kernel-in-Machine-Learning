26e2f020d21cb971b9349eac9521747be42ecc78
charlesAuguste <charles.auguste@ymail.com>
2019-03-25 23:33:15 +0100

    Sum kernel.

--- kernels/sum_kernel.py
+++ kernels/sum_kernel.py
@@ -3,26 +3,10 @@ import numpy as np
 from kernels.fast_spectrum_kernel import SpectrumKernel
 import scipy.sparse as ss
 
-class SumSpectrumKernel(FastKernel):
-    def __init__(self, list_k):
+class SumKernel():
+    def __init__(self, kernels):
         super().__init__()
-        self.list_k = list_k
-        self.index = {"A": 0, "C": 1, "G": 2, "T": 3}
-        self.kernels = [SpectrumKernel(k, normalize=False) for k in list_k]
-
-    def compute_index(self, word):
-        for i in range(len(word)):
-            word[i] = self.index[word[i]]
-        return np.dot(np.array(word), np.power(4, np.arange(self.list_k)))
-
-    def compute_feature_vector(self, X):
-        # TODO Maybe need sparse matrix for k very large
-        features = self.kernels[0].compute_feature_vector(X)
-        for i in range(1, len(self.kernels)):
-            kernel_feature = self.kernels[i].compute_feature_vector(X)
-            features = ss.vstack(([features, kernel_feature])).toarray()
-        print("Shape of features from SSK: {}".format(features.shape))
-        return features
+        self.kernels = kernels
 
     def compute_train(self, data_train):
         K = 0
