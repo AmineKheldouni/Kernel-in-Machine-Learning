abdc5fe87ba9a5f1308e94eeff9415d30eed93b5
AmineKheldouni <aminekheldouni@gmail.com>
2019-03-28 15:51:18 +0100

    Fix split=0 and add kfold cross validation

--- utils.py
+++ utils.py
@@ -13,9 +13,41 @@ def SVM_prediction(data_train, data_val, y_train, y_val, kernel, lbd=0.001):
     return svm, svm.score_train(), (y_val == predictions).mean()
 
 def train_val_split(data, y, split = 300):
+    if split == 0:
+        return data, np.array([]), y, np.array([])
     permutation = np.random.permutation(len(data))
     y_train = y[permutation][:-split]
     y_test = y[permutation][-split:]
     data_train = data[permutation][:-split]
     data_test = data[permutation][-split:]
     return data_train, data_test, y_train, y_test
+
+def kfold_cross_validation(data, predictions, kernels, list_lambda, k=10):
+
+    nfold = len(data) // k
+    scores = np.zeros((k, len(list_lambda), 3))
+    for d in range(3):
+        permutation = np.random.permutation(len(data[d]))
+        predictions_bucket = list(predictions[d][permutation])
+        data_bucket = list(data[d][permutation])
+        for j in range(len(list_lambda)):
+            for i in range(k):
+                print("##############################")
+                print("Performing Fold {} (lambda={})".format(i+1,list_lambda[j]))
+                data_val = np.array(data_bucket[i*nfold:(i+1)*nfold])
+                y_val = np.array(predictions_bucket[i*nfold:(i+1)*nfold])
+                data_train = np.array(data_bucket[:i*nfold] + data_bucket[(i+1)*nfold:])
+                y_train = np.array(predictions_bucket[:i*nfold] + predictions_bucket[(i+1)*nfold:])
+                svm, train_acc, val_acc = SVM_prediction(data_train,
+                                                         data_val,
+                                                         y_train,
+                                                         y_val,
+                                                         kernels[d],
+                                                         list_lambda[j])
+                print("Training accuracy:", train_acc)
+                print("Valudation accuracy:", val_acc)
+                print("######")
+                scores[i,j,d] = val_acc
+
+    print("####################################")
+    return scores, np.mean(scores,axis=0)
